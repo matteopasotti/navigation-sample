a03fb0f6a9807f6c01aacc23ec2a03612e7857d8
Matteo Pasotti <matteopasotti1@gmail.com>
2019-05-12 11:15:27 +0100

    init main activity with navigation graph

--- app/src/main/java/com/example/navigationsample/MainActivity.kt
+++ app/src/main/java/com/example/navigationsample/MainActivity.kt
@@ -1,6 +1,20 @@
 package com.example.navigationsample
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.navigationsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        // Link the NavController to our ActionBar.
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
 }
