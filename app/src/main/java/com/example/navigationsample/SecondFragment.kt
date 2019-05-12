c7f3bf0f164ce3458f35454bf820eb73f7f390fa
Matteo Pasotti <matteopasotti1@gmail.com>
2019-05-12 13:00:50 +0100

    developed shared element transition sample

--- app/src/main/java/com/example/navigationsample/SecondFragment.kt
+++ app/src/main/java/com/example/navigationsample/SecondFragment.kt
@@ -6,11 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.FragmentNavigatorExtras
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.example.navigationsample.databinding.FragmentSecondBinding
 
 class SecondFragment : Fragment() {
 
     lateinit var binding : FragmentSecondBinding
+
+    val IMAGE_URI = "https://koenig-media.raywenderlich.com/uploads/2018/07/Jetpack-feature.png"
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -19,6 +24,18 @@ class SecondFragment : Fragment() {
 
         var args = arguments?.let { SecondFragmentArgs.fromBundle(it) }
         binding.txtArgumentValue.text = args!!.textInput
+
+        binding.btnNext.setOnClickListener {
+
+            val extras = FragmentNavigatorExtras(
+                binding.image to "imageView"
+            )
+
+            findNavController().navigate(R.id.action_secondFragment_to_thirdFragment , null, null , extras)
+        }
+
+
+        Glide.with(context!!).load(IMAGE_URI).centerCrop().into(binding.image)
 
 
         return binding.root
---
+++ app/src/main/java/com/example/navigationsample/ThirdFragment.kt
@@ -0,0 +1,39 @@
+package com.example.navigationsample
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.transition.TransitionInflater
+import com.bumptech.glide.Glide
+import com.example.navigationsample.databinding.FragmentThirdBinding
+
+class ThirdFragment : Fragment() {
+
+    val IMAGE_URI = "https://koenig-media.raywenderlich.com/uploads/2018/07/Jetpack-feature.png"
+
+    lateinit var binding : FragmentThirdBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        binding = DataBindingUtil.inflate<FragmentThirdBinding>(inflater, R.layout.fragment_third, container, false)
+
+
+        return binding.root
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        sharedElementEnterTransition = TransitionInflater.from(context).inflateTransition(android.R.transition.move)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //activity.setupActionBarWithNavController(findNavController())
+        Glide.with(this).load(IMAGE_URI).into(binding.headerImage)
+    }
+}
