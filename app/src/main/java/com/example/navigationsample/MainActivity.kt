6540f8420d701d445414b9e212ff756a7c80595f
Matteo Pasotti <matteopasotti1@gmail.com>
2019-05-18 16:01:09 +0100

    developed menu with navigation drawer

--- app/src/main/java/com/example/navigationsample/MainActivity.kt
+++ app/src/main/java/com/example/navigationsample/MainActivity.kt
@@ -1,23 +1,30 @@
 package com.example.navigationsample
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.navigationsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var drawerLayout : DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        drawerLayout = binding.drawerLayout
 
 
         // Link the NavController to our ActionBar.
         val navController = this.findNavController(R.id.myNavHostFragment)
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController , drawerLayout)
+        // Set up the navigation to know about the NavigationView
+        NavigationUI.setupWithNavController(binding.navView , navController)
     }
 
     // here we specify what happens when we press the up button (arrow on top left)
@@ -25,6 +32,8 @@ class MainActivity : AppCompatActivity() {
     // To do this we override onSupportNavigateUp, find the nav controller, and then we call navigateUp().
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
-        return navController.navigateUp()
+        return NavigationUI.navigateUp( navController , drawerLayout)
     }
+
+
 }
